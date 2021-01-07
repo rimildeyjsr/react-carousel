56bf7b38710e4be5dcc34439c36cd85e7fe0f19d
Rimil Dey <rimildeyjsr@gmail.com>
2021-01-07 12:28:06 +0530

    Fixed scrolling with animation

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,53 +1,58 @@
-import React, {useState, useRef} from 'react';
+import React, {useState} from 'react';
 import CarouselItem from "../carousel-item";
 import {bookItemList} from "../../constants/book-list";
 import "./index.css";
 
 const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(1);
-  const containerRef = useRef();
+  const [direction, setDirection] = useState(0);
   const totalBooks = bookItemList.length;
 
   const moveToPreviousIndex = () => {
     const previousIndex = activeIndex - 1;
     if(previousIndex - 1 >= 0) {
-      containerRef.current.children[previousIndex].scrollIntoView({ behavior: "smooth", inline: "center", block: "center" });
       setActiveIndex(previousIndex);
+      setDirection(direction + 392);
     }
   };
 
   const moveToNextIndex = () => {
     const nextIndex = activeIndex + 1;
     if(nextIndex + 1 < totalBooks) {
-      containerRef.current.children[nextIndex].scrollIntoView({ behavior: "smooth", inline: "center", block: "center" });
       setActiveIndex(nextIndex);
+      setDirection(direction - 392);
     }
   };
 
   return (
-    <div className="carousel center-align-flex">
-      {
-        totalBooks > 0 ?
-          <div className="carousel-wrapper center-align-flex" ref={containerRef}>
-            {
-              bookItemList.map((book, index) => {
-                return (
-                  <CarouselItem
-                    bookInfo={book}
-                    middleElement={index === activeIndex}
-                    key={book.key}
-                  />
-                )
-              })
-            }
-          </div>
+    <>
+      <div className="carousel">
+        {
+          totalBooks > 0 ?
+            <div
+              className="carousel-wrapper"
+              style={{transform: 'translateX(' + direction + 'px'}}
+            >
+              {
+                bookItemList.map((book, index) => {
+                  return (
+                    <CarouselItem
+                      bookInfo={book}
+                      middleElement={index === activeIndex}
+                      key={book.key}
+                    />
+                  )
+                })
+              }
+            </div>
 
-          :
+            :
 
-          <h3>
-            No items to show here!
-          </h3>
-      }
+            <h3>
+              No items to show here!
+            </h3>
+        }
+      </div>
 
       <div className="carousel-arrow-wrapper">
         <button onClick={moveToPreviousIndex}>
@@ -58,7 +63,7 @@ const Carousel = () => {
           {">"}
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
